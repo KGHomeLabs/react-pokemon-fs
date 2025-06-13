1bdf61f6af863403f58e11b129be436ba1abad8e
KGExperiments <noplan422@gmail.com>
2025-06-13 15:23:18 -0400

    trying to access environment variables still failing

--- react-pokemon-client/src/env.ts
+++ react-pokemon-client/src/env.ts
@@ -7,10 +7,18 @@ export const AppEnv = {
 
 export type AppEnv = (typeof AppEnv)[keyof typeof AppEnv];
 
+function getEnvironmentName(): string | undefined {
+    return import.meta.env.VITE_APP_ENV;
+}
 
 export function getAppEnv(): AppEnv{
-    const env = import.meta.env.VITE_APP_ENV as AppEnv | undefined;
-    return env ?? AppEnv.Development;
+    const env = getEnvironmentName();
+    if (env === AppEnv.Production) 
+        return AppEnv.Production;
+    if (env === AppEnv.Preview) 
+        return AppEnv.Preview;
+    //the else case
+    return AppEnv.Development;
 }
 
 export function isDev() {
--- react-pokemon-client/vite.config.ts
+++ react-pokemon-client/vite.config.ts
@@ -1,10 +1,55 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { execSync } from 'child_process'
+import { AppEnv } from './src/env'
+
+
+export const GitBranch = {
+  Development: 'dev',
+  Preview: 'prev',
+  Production: 'master',
+} as const;
+
+export type GitBranch = (typeof GitBranch)[keyof typeof GitBranch];
+
+const resolveAppEnv = (mode:string): AppEnv => {
+  if (mode === 'development') {
+    //here I would like to do something special when project is launched
+    //using `npm run dev` or `yarn dev`
+    //ensuring that the environment switches also work when not on 
+    //hosting environments like Vercel or Netlify, cloudflare etc.
+    //this will be tied to the branch that yarn dev is ran from
+    try {
+      const branch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
+      if (branch === GitBranch.Production)
+        return AppEnv.Production;
+      if (branch === GitBranch.Preview)
+        return AppEnv.Preview;
+      return AppEnv.Development;
+
+    } catch {
+      //defaults to development
+      //todo add a log entry to make sure this will be seen
+      return AppEnv.Development;
+    }
+  } else {
+    //in case we are not in vite development mode...
+    return process.env.VITE_APP_ENV as AppEnv;
+  }
+}
+
+
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-   build: {
-    sourcemap: true
+export default defineConfig(({mode})=>{
+  const currentAppEnv = resolveAppEnv(mode);
+  return {
+    plugins: [react()],
+    define: {
+      'import.meta.env.VITE_APP_ENV':JSON.stringify(currentAppEnv)
+    },
+    build: {
+      sourcemap: true
+    }
   }
 })
