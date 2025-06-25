45f900d939fd9cbacd288d140627d889bbbc9405
Home lab builder <NoPlan422@gmail.com>
2025-06-24 22:16:45 -0400

    changed the httpclient to fetch and it now supports the abort controller relates to #65

---
+++ react-pokemon-client/src/lib/http-client/http-axios.ts
@@ -0,0 +1,45 @@
+import { injectable} from 'tsyringe';
+
+import type { AxiosInstance, AxiosRequestConfig,AxiosResponse } from 'axios';
+import type {IHttpClientAxios} from './i-http-client';
+
+type HttpMethod = 'get' | 'post' | 'put' | 'patch' | 'delete';
+
+@injectable()
+export default class HttpClient implements IHttpClientAxios {
+  private client: AxiosInstance;
+
+  constructor(client: AxiosInstance) {
+    this.client = client;
+  }
+
+  private async request<T>(method: HttpMethod, url: string, data?: unknown, config?: AxiosRequestConfig): Promise<T> {
+    try {
+      const response: AxiosResponse<T> = await this.client.request<T>({ method, url, data, ...config });
+      return response.data;
+    } catch (error) {
+      console.error(`[HttpClient] ${method.toUpperCase()} ${url}`, error);
+      throw error;
+    }
+  }
+
+  public get<T>(url: string, config?: AxiosRequestConfig): Promise<T> {
+    return this.request<T>('get', url, undefined, config);
+  }
+
+  public post<T>(url: string, data?: unknown, config?: AxiosRequestConfig): Promise<T> {
+    return this.request<T>('post', url, data, config);
+  }
+
+  public put<T>(url: string, data?: unknown, config?: AxiosRequestConfig): Promise<T> {
+    return this.request<T>('put', url, data, config);
+  }
+
+  public patch<T>(url: string, data?: unknown, config?: AxiosRequestConfig): Promise<T> {
+    return this.request<T>('patch', url, data, config);
+  }
+
+  public delete<T>(url: string, config?: AxiosRequestConfig): Promise<T> {
+    return this.request<T>('delete', url, undefined, config);
+  }
+}
--- react-pokemon-client/src/lib/http-client/i-http-client.ts
+++ react-pokemon-client/src/lib/http-client/i-http-client.ts
@@ -2,10 +2,22 @@ import type { AxiosRequestConfig } from 'axios';
 
 export const IHttpClientServiceToken = Symbol.for('IHttpClientService');
 
-export default interface IHttpClientService {
+export interface IHttpClientService {
+  get<T>(url: string, config?: RequestInit): Promise<T>;
+  post<T>(url: string, body?: unknown, config?: RequestInit): Promise<T>;
+  put<T>(url: string, body?: unknown, config?: RequestInit): Promise<T>;
+  patch<T>(url: string, body?: unknown, config?: RequestInit): Promise<T>;
+  delete<T>(url: string, config?: RequestInit): Promise<T>;
+  setBaseUrl(url: string): void;
+}
+
+//probably deprecated, I will unify the interface with the axios one
+// this is the interface for the axios client, it should be used in the services that use axios
+//but it has issues, now abort controller is not supported, so I will use the native fetch
+export interface IHttpClientAxios {
   get<T>(url: string, config?: AxiosRequestConfig): Promise<T>;
-  post<T>(url: string, data?: unknown, config?: AxiosRequestConfig): Promise<T>;
-  put<T>(url: string, data?: unknown, config?: AxiosRequestConfig): Promise<T>;
-  patch<T>(url: string, data?: unknown, config?: AxiosRequestConfig): Promise<T>;
+  post<T>(url: string, body?: unknown, config?: AxiosRequestConfig): Promise<T>;
+  put<T>(url: string, body?: unknown, config?: AxiosRequestConfig): Promise<T>;
+  patch<T>(url: string, body?: unknown, config?: AxiosRequestConfig): Promise<T>;
   delete<T>(url: string, config?: AxiosRequestConfig): Promise<T>;
 }
