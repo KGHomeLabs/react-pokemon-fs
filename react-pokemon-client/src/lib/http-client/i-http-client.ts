b763d5034eb73f4e29032ea561efdcf4b498dfca
Home lab builder <NoPlan422@gmail.com>
2025-06-24 23:36:19 -0400

    removed unnecessary imports #65

--- react-pokemon-client/src/lib/http-client/i-http-client.ts
+++ react-pokemon-client/src/lib/http-client/i-http-client.ts
@@ -1,7 +1,7 @@
-import type { AxiosRequestConfig } from 'axios';
 
 export const IHttpClientServiceToken = Symbol.for('IHttpClientService');
 
+// Interface for HTTP client service
 export interface IHttpClientService {
   get<T>(url: string, config?: RequestInit): Promise<T>;
   post<T>(url: string, body?: unknown, config?: RequestInit): Promise<T>;
