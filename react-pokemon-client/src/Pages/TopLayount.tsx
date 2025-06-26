3b33dadf0d7a282128a36ce965f9091823e94295
Home lab builder <NoPlan422@gmail.com>
2025-06-26 18:17:25 -0400

    changed environment label so its not in the way of the userButton later#74 relates to #71 in context of wp: #67 , #24

--- react-pokemon-client/src/Pages/TopLayount.tsx
+++ react-pokemon-client/src/Pages/TopLayount.tsx
@@ -1,9 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import { Header } from '../blox/features/header/Header'
+import { getAppEnv } from '../../config/env-switch'
+import EnvLabel from '../blox/features/env-label/EnvLabel'
 
 export default function TopLayout() {
+    const computedEnv = getAppEnv()
     return (
         <div>
+            <EnvLabel computedEnv={computedEnv}></EnvLabel>
             <Header />
             <main style={{ padding: '1rem' }}>
                 <Outlet />
